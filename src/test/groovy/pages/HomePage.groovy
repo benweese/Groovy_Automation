1941069dd6a86861be12810ebbb1e30044226c0d
Ben Weese <Ben.weese@ameritas.com>
2019-08-27 14:54:33 -0500

    change to non template

--- src/test/groovy/pages/HomePage.groovy
+++ src/test/groovy/pages/HomePage.groovy
@@ -4,7 +4,7 @@ import geb.Page
 import modules.SizeMenuModule
 
 class HomePage extends Page{
-    static at = { title == "Home - BenWeese.Dev" }
+    static at = { title == "React Shopping Cart" }
 
     static content = {
         sizeMenu { module SizeMenuModule }
